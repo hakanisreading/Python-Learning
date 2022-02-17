2650da397dbb88cc21c9233637fa9339c482241a
Hakan Yaraş <83615331+hakanisreading@users.noreply.github.com>
2022-02-17 15:52:41 +0300

    if-elif yapıları

---
+++ if-elif.py
@@ -0,0 +1,20 @@
+# x = 10
+# y = 20
+
+# if (x > y):
+#     print("X, Y den büyüktür")
+# elif (x == y):
+#     print("X ile Y eşittir")
+# else:
+#     print("Y, X den büyüktür")
+
+
+
+
+sayi = int(input("Sayı giriniz: "))
+if sayi > 0:
+    print("Sayı pozitiftir")
+elif sayi == 0:
+    print("Sayı sıfırdır")
+else:
+    print("Sayı negatiftir")
