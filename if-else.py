6ca4424cf0236f1013c497ac83c85658fa8ccdb6
Hakan Yaraş <83615331+hakanisreading@users.noreply.github.com>
2022-02-17 15:52:14 +0300

    if-else yapıları

---
+++ if-else.py
@@ -0,0 +1,27 @@
+# if (5 > 3):
+#     print("Merhaba")
+# isLoggedin = True
+# if (isLoggedin == True):
+#     print("Merhaba")
+
+
+# username = "hakan"
+# password = "1234"
+
+# isLoggedin = (username == "hakan" and password == "12345")
+# if isLoggedin:
+#     print("Hoşgeldin " + username)
+# else:
+#     print("username ya da parola yanlış")
+
+
+username = "hakan"
+password = "1234"
+
+if (username == "hakan"):
+    if (password == "1234"):
+        print("Uygulamaya hoşgeldiniz")
+    else:
+        print("Parola yanlış")
+else:
+    print("Kullanıcı adı yanlış")
