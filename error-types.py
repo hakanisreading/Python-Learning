587b9830e463021044b1628c692d03e1dfbe8b30
Hakan Yaraş <83615331+hakanisreading@users.noreply.github.com>
2022-05-30 14:09:48 +0300

    Hata Türleri

---
+++ error-types.py
@@ -0,0 +1,35 @@
+# x = int(input("x: "))
+# y = int(input("y: "))
+
+# print(x / y)
+
+# Syntax error = yazım yanlışı
+# kjdfhgjkdh;;
+
+# def yazdir((
+#     pass
+
+# print("merhab"a) 
+
+# nameError = tanımlanmamış bir değişken kullanımı
+# print(ad)
+
+# typeError = hatalı parametre kullanımı 
+# len(5)
+# 4 + "a"
+
+# indexError = yanlış index numarası
+# liste = ["merhaba"]
+# liste[1]
+
+# valueError = hatalı tip kullanımı
+# int("10a")
+
+# keyError = key hatası
+# d = {}
+# d["ad"]
+
+# attributeError = olmayan bir özelliğe ulaşmak istediğimizde 
+# "merhaba".upper()
+# "merhaba".Upper
+
